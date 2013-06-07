0702ea5a77a8f70c92a02020e116b5ecc17510be
Lou Rinaldi <louis.rinaldi@yale.edu>
2013-06-07 11:17:17 -0400

    use 2.9.2 source instead of trunk

--- modules/sakai/manifests/subversion.pp
+++ modules/sakai/manifests/subversion.pp
@@ -6,7 +6,7 @@ class sakai::subversion {
   vcsrepo { "/vagrant/sakai-src":
     ensure   => present,
     provider => svn,
-    source   => "https://source.sakaiproject.org/svn/sakai/trunk",
+    source   => "https://source.sakaiproject.org/svn/sakai/tags/sakai-2.9.2/",
     require  => Package["subversion"],
   }
 }
